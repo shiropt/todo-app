6a505f463b1a61901f8ea69a67dfb7b520710de7
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-07 07:29:14 +0900

    create button components and tests

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { Button } from ".";
+import styled from "styled-components";
+
+const meta = {
+  title: "atoms/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {
+    children: "Button",
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Buttons: StoryFn = () => {
+  return (
+    <Style>
+      <Button>Default</Button>
+      <Button rounded>rounded</Button>
+      <Button large>large</Button>
+      <Button large rounded>
+        large rounded
+      </Button>
+    </Style>
+  );
+};
+
+const Style = styled.div`
+  > * {
+    display: block;
+    margin: 8px;
+    text-align: center;
+  }
+`;
