0237ec2901dc857b6a0afd457618054db066f627
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-07 09:48:44 +0900

    add test msw of browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import styled from "styled-components";
@@ -18,6 +18,12 @@ const Logo = styled.img`
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/todos")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <>
