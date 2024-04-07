45f1c7e62e4e7e47e6390735099f8f534cd521ce
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-07 09:49:58 +0900

    change localhost port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     react({
       plugins: [
