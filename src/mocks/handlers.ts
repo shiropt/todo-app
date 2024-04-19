7d74cea751112cfdf7f7c60e5c3ca67ad99c248c
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-20 08:51:59 +0900

    fix mock handlers to http method

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,6 @@
+import { Todo, TodosResponse, TodoResponse } from "./../modules/todo/type";
 import { db } from "./db";
+import { http, HttpResponse } from "msw";
 
 for (let i = 1; i <= 100; i++) {
   db.todo.create({
@@ -7,9 +9,74 @@ for (let i = 1; i <= 100; i++) {
     status: Math.floor(Math.random() * 3),
     description: `これはタスク #${i} の説明です。`,
     deadline: `2024-12-${String((i % 30) + 1).padStart(2, "0")}`, // 月の日付を模倣
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
   });
 }
 
-export const handlers = [...db.todo.toHandlers("rest")];
+export const handlers = [
+  http.get<Record<string, string>, Record<string, string>, TodosResponse>(
+    "/todos",
+    () => {
+      const todos = db.todo.getAll();
+
+      return HttpResponse.json({
+        type: "todo",
+        status: 200,
+        data: todos as TodosResponse["data"],
+      });
+    }
+  ),
+  http.get<{ id: string }, Todo, TodoResponse>("/todo/:id", ({ params }) => {
+    const { id } = params;
+    const data = db.todo.findFirst({ where: { id: { equals: id } } }) as Todo;
+    return HttpResponse.json({
+      type: "todo",
+      status: 200,
+      data,
+    });
+  }),
+  http.post<{ id: string }, Todo, TodoResponse>(
+    "/todo",
+    async ({ request }) => {
+      const todo = await request.json();
+      const data = db.todo.create({
+        id: String(db.todo.getAll().length + 1),
+        title: todo.title,
+        status: todo.status,
+        description: todo.description,
+        deadline: todo.deadline,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      }) as Todo;
+      return HttpResponse.json({ type: "todo", status: 201, data });
+    }
+  ),
+  http.put<{ id: string }, Todo, TodoResponse>(
+    "/todo/:id",
+    async ({ request, params }) => {
+      const { id } = params;
+      const todo = await request.json();
+      const data = db.todo.update({
+        where: { id: { equals: id } },
+        data: {
+          title: todo.title,
+          status: todo.status,
+          description: todo.description,
+          deadline: todo.deadline,
+          updated_at: new Date().toISOString(),
+        },
+      }) as Todo;
+      return HttpResponse.json({ type: "todo", status: 204, data });
+    }
+  ),
+  http.delete<{ id: string }, Todo, TodoResponse>("/todo/:id", ({ params }) => {
+    const { id } = params;
+    const data = db.todo.delete({ where: { id: { equals: id } } }) as Todo;
+    return HttpResponse.json({
+      type: "todo",
+      status: 200,
+      data,
+    });
+  }),
+];
