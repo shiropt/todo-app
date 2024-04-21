095dc734232dbc80e2127cb7150ff660eea43654
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-21 09:53:45 +0900

    create variables

---
+++ src/styles/variables.ts
@@ -0,0 +1,12 @@
+export const SOFT_GREY = "#f3f4f6;";
+export const TRANSLUCENT_GREY = "#dbdcdf25;";
+export const SLATE_GREY = "#6b7280;";
+export const INKY_BLACK = "#111827";
+
+export const aliases = {
+  border: "1px solid #e5e7eb;",
+  hoverBackgroundColor: SOFT_GREY,
+  backgroundColor: TRANSLUCENT_GREY,
+  primaryColor: INKY_BLACK,
+  inActiveColor: SLATE_GREY,
+};
