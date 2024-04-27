e9c6677b275dfe462a0713a2144de7b8bce47f14
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-28 06:44:56 +0900

    setting config for absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -15,5 +16,6 @@ export default defineConfig({
         ],
       ],
     }),
+    tsconfigPaths(),
   ],
 });
