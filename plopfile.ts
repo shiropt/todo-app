a58b4ec67a57544025a7b9272686eaf3a8c5b5b1
shiropt <63380875+shiropt@users.noreply.github.com>
2024-04-27 09:26:47 +0900

    create plopfile

--- plopfile.ts
+++ plopfile.ts
@@ -2,16 +2,51 @@
 import { NodePlopAPI } from "plop";
 
 export default function (plop: NodePlopAPI) {
-  // plop generator code
   plop.setGenerator("controller", {
     description: "application controller logic",
     prompts: [
       {
         type: "input",
         name: "name",
-        message: "controller name please",
+        message: "component name please",
+      },
+      {
+        type: "list",
+        name: "directory",
+        message: "select component directory",
+        choices: ["atoms", "molecules", "organisms", "layouts", "pages"],
+      },
+      {
+        type: "confirm", // 新しいプロンプトのタイプは"confirm"です
+        name: "createTestFile", // テストファイルを作成するかどうかを示すフラグ
+        message: "Do you want to create a test file?", // メッセージ
+        default: true, // デフォルトで"yes"を選択します
       },
     ],
-    actions: [],
+    actions: (data) => {
+      const actions = [
+        {
+          type: "add",
+          path: `src/components/{{directory}}/{{pascalCase name}}/{{pascalCase name}}.tsx`,
+          templateFile: "src/templates/base.hbs",
+        },
+        {
+          type: "add",
+          path: `src/components/{{directory}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx`,
+          templateFile: "src/templates/stories.hbs",
+        },
+        {
+          type: "add",
+          path: `src/components/{{directory}}/{{pascalCase name}}/index.ts`,
+          templateFile: "src/templates/index.hbs",
+        },
+        {
+          type: "add",
+          path: `src/components/{{directory}}/{{pascalCase name}}/{{pascalCase name}}.spec.tsx`,
+          templateFile: "src/templates/spec.hbs",
+        },
+      ];
+      return data?.createTestFile ? actions : actions.slice(0, 3);
+    },
   });
 }
