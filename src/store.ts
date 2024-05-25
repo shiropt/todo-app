87990823b0fa5df7bec47bb8780476c80b46cd5e
shiropt <63380875+shiropt@users.noreply.github.com>
2024-05-25 23:03:06 +0900

    fix: remove unnecessary code

--- src/store.ts
+++ src/store.ts
@@ -12,12 +12,3 @@ export const store = configureStore({
 });
 
 setupListeners(store.dispatch);
-
-const data = [];
-data.push(1);
-
-const foo = require("foo");
-const original = ["foo", "bar", "baz"];
-const sorted = [...original].sort((a, b) => a.localeCompare(b)); // This is OK with ignoreImmediateMutation.
-
-window.alert("Hello, world!");
