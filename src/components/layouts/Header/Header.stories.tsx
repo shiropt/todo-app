2097fae6607d43c07c9d6c019f910075d1e42474
shiropt <63380875+shiropt@users.noreply.github.com>
2024-05-26 14:25:08 +0900

    replaced Header with radix-ui Theme

--- src/components/layouts/Header/Header.stories.tsx
+++ src/components/layouts/Header/Header.stories.tsx
@@ -21,6 +21,7 @@ export const Default: Story = {
 export const WidthImage: Story = {
   args: {
     className: "header",
-    src: "https://img.freepik.com/free-photo/portrait-young-woman-wearing-glasses-3d-rendering_1142-43632.jpg?size=626&ext=jpg",
+    avatarImage:
+      "https://img.freepik.com/free-photo/portrait-young-woman-wearing-glasses-3d-rendering_1142-43632.jpg?size=626&ext=jpg",
   },
 };
--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,58 +1,37 @@
+import { IconButton } from "@/components/atoms/IconButton";
+import { Avatar, Flex } from "@radix-ui/themes";
 import type { FC } from "react";
 import styled from "styled-components";
-import { IconButton } from "@/components/atoms/IconButton";
-import * as Avatar from "@radix-ui/react-avatar";
 
 type Props = {
   className?: string;
-  src?: string;
+  avatarImage?: string;
   alt?: string;
 };
 
-const StyledAvatar = styled(Avatar.Image)`
-  width: 26px;
-  height: 26px;
-  object-fit: cover;
-  border-radius: 50%;
-`;
-
-export const Header: FC<Props> = ({ className, src, alt }) => {
+export const Header: FC<Props> = ({ className, avatarImage, alt }) => {
   return (
     <StyledHeader className={className}>
-      <IconButton icon="mdiMagnify" rounded />
-      <div>
-        <IconButton icon="mdiBellOutline" rounded />
-        <Avatar.Root>
-          <StyledAvatar className={className} alt={alt} src={src} />
-          <Avatar.Fallback>
-            <IconButton icon="mdiAccountOutline" rounded />
-          </Avatar.Fallback>
-        </Avatar.Root>
-      </div>
+      <Flex height="100%" justify="between" align="center">
+        <IconButton icon="mdiMagnify" />
+        <Flex align="center" gap="4">
+          <IconButton icon="mdiBellOutline" />
+          <Avatar
+            src={avatarImage}
+            alt={alt}
+            radius="full"
+            size="2"
+            fallback={<IconButton icon="mdiAccountOutline" />}
+          />
+        </Flex>
+      </Flex>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
   height: 60px;
+  padding: 0 16px;
   background-color: ${({ theme }) => theme.background.main};
   border-bottom: ${({ theme }) => theme.border};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  > button {
-    height: 32px;
-    margin-left: 16px;
-  }
-  > div {
-    display: flex;
-    margin-right: 8px;
-    align-items: center;
-    button,
-    img {
-      display: block;
-      margin: 0 8px;
-    }
-  }
 `;
