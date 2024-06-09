f5726f23062a7c41eb377eb9b540c21c60745200
shiropt <63380875+shiropt@users.noreply.github.com>
2024-06-09 23:01:54 +0900

    feat: add to response value

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -18,7 +18,7 @@ for (let i = 1; i <= 100; i++) {
     title: `タスク #${i}`,
     status: Math.floor(Math.random() * 3),
     description: `これはタスク #${i} の説明です。`,
-    deadline: `2024-12-${String((i % 30) + 1).padStart(2, "0")}`, // 月の日付を模倣
+    deadline: new Date().toISOString(), // 月の日付を模倣
     created_at: new Date().toISOString(),
     updated_at: new Date().toISOString(),
   });
