58a6c030f5f92db85e4317508f2f847cb76dd395
shiropt <63380875+shiropt@users.noreply.github.com>
2024-06-09 23:05:03 +0900

    feat: replaced Menu Conponent to mantine-ui

--- src/components/molecules/Menu/Menu.tsx
+++ src/components/molecules/Menu/Menu.tsx
@@ -1,45 +1,26 @@
 import type { FC } from "react";
-import styled from "styled-components";
 import { Icon } from "@/components/atoms/Icon";
-import { Link } from "wouter";
+import { Link, useLocation } from "wouter";
 import { Menus } from "@/routes/navigation";
+import { NavLink } from "@mantine/core";
 
 type Props = Menus[number];
 
 export const Menu: FC<Props> = ({ icon, title, to }) => {
+  const [location] = useLocation();
+
   return (
-    <StyledMenu>
-      <Link className={(active) => (active ? "active" : "")} to={to}>
-        <Icon icon={icon} />
-        <span>{title}</span>
-      </Link>
-    </StyledMenu>
+    <Link
+      style={{ textDecoration: "none", color: "inherit", width: "100%" }}
+      to={to}
+    >
+      <NavLink
+        active={location === to}
+        w="100%"
+        leftSection={<Icon icon={icon} />}
+        p="md"
+        label={title}
+      />
+    </Link>
   );
 };
-
-const StyledMenu = styled.li`
-  color: ${({ theme }) => theme.colors.passive};
-  border-radius: 4px;
-  cursor: pointer;
-  > a {
-    display: block;
-    text-decoration: none;
-    color: inherit;
-    padding: 8px;
-    /* padding: inherit; li 要素の padding を継承 */
-    > svg,
-    span {
-      vertical-align: middle;
-      margin: 0 4px;
-    }
-  }
-  > a:hover {
-    color: ${({ theme }) => theme.colors.active};
-    background: ${({ theme }) => theme.background.hover};
-    transition: background 0.3s;
-  }
-  .active {
-    color: ${({ theme }) => theme.colors.active};
-    background-color: ${({ theme }) => theme.background.active};
-  }
-`;
