096299f33bfbfd2cc80dec682af4938e6c527760
shiropt <63380875+shiropt@users.noreply.github.com>
2024-06-09 23:02:27 +0900

    feat: create ActionIcon Component

---
+++ src/components/atoms/ActionIcon/ActionIcon.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ActionIcon } from "./ActionIcon";
+
+const meta = {
+  title: "atoms/ActionIcon",
+  component: ActionIcon,
+  parameters: {},
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof ActionIcon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    icon: "mdiAccountOutline",
+  },
+};
