79f8e5669cbb1717127f7bdca62e278489e9005c
63380875+shiropt@users.noreply.github.com. <shiro19880610@yahoo.co.jp>
2024-08-11 13:27:46 +0900

    fix: storybook error

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,10 @@
+import { Provider as ReduxProvider } from "react-redux";
 import { MantineProvider, MantineThemeOverride } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import type { Preview } from "@storybook/react";
 import React from "react";
+import { store } from "../src/store";
 
 export const theme: MantineThemeOverride = {};
 
@@ -19,9 +21,11 @@ const preview: Preview = {
 
 export const decorators = [
   (renderStory: any) => (
-    <MantineProvider forceColorScheme="light" theme={theme}>
-      {renderStory()}
-    </MantineProvider>
+    <ReduxProvider store={store}>
+      <MantineProvider forceColorScheme="light" theme={theme}>
+        {renderStory()}
+      </MantineProvider>
+    </ReduxProvider>
   ),
 ];
 
