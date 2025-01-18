4bbd6407871a4462f956b382a58808e09efe48a1
shiropt <63380875+shiropt@users.noreply.github.com>
2025-01-18 16:51:40 +0900

    comment out unnecessary route

--- src/routes/navigation.ts
+++ src/routes/navigation.ts
@@ -3,11 +3,11 @@ import { paths } from "@/routes/path";
 export const menus = [
   { title: "Home", to: `${paths.home}`, icon: "mdiHomeOutline" },
   { title: "Todos", to: `${paths.todos}`, icon: "mdiFileTreeOutline" },
-  {
-    title: "Calendar",
-    to: `${paths.calendar}`,
-    icon: "mdiCalendarTextOutline",
-  },
+  // {
+  //   title: "Calendar",
+  //   to: `${paths.calendar}`,
+  //   icon: "mdiCalendarTextOutline",
+  // },
   // {
   //   title: "Stats",
   //   to: `${paths.stats}`,
