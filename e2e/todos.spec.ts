76e87700ec4a58330720cf8e546510e208dc42f0
shiropt <63380875+shiropt@users.noreply.github.com>
2025-03-23 08:10:11 +0900

    CircleCI: add test for redirecting to /home on accessing a non-existent URL

--- e2e/todos.spec.ts
+++ e2e/todos.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
 
+test("存在ないURLにアクセスすると/homeに遷移する", async ({ page }) => {
+  await page.goto("http://localhost:3000/example");
+  await expect(page).toHaveURL("http://localhost:3000/home");
+});
 test("Todos メニュークリックで todos ページへ遷移する", async ({ page }) => {
   await page.goto("http://localhost:3000/");
   await page.getByRole("button", { name: "Todos" }).click();
